2a3db19cf4eacf75375c14f4d0897deb42c7d4b7
joshcallawaydev <joshrcal89@gmail.com>
2021-08-10 19:40:57 +0000

    Added register function and form

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import os
 from flask import (
     Flask, render_template, url_for,
     flash, redirect, request, session)
-
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -14,10 +16,37 @@ app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
 
+mongo = PyMongo(app)
+
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template("home.html")
+
+
+@app.route("/register", methods=["GET", "POST"])
+def register():
+    if request.method == "POST":
+        # check if user already exists
+        existing_user = mongo.db.users.find_one(
+            {"username": request.form.get("username").lower()})
+
+        if existing_user:
+            flash("User already exists")
+            return redirect(url_for("register"))
+        register = {
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+
+        # put user in session cookie
+        session["user"] = request.form.get("username").lower()
+        flash("Registration Successful")
+        return redirect(url_for("home", username=session["user"]))
+    # change this to dashboard once dashboard.html is created
+    return render_template("register.html")
 
 
 if __name__ == "__main__":
