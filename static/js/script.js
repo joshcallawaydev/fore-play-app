4c0ca72fe2496f09a62cf60a9a24d2185e236b31
joshcallawaydev <joshrcal89@gmail.com>
2021-08-10 08:11:23 +0000

    Removed vanilla js, using jQuery

--- static/js/script.js
+++ static/js/script.js
@@ -5,8 +5,3 @@ $(".dropdown-trigger").dropdown();
 $(document).ready(function () {
     $('.sidenav').sidenav();
 });
-
-document.addEventListener('DOMContentLoaded', function () {
-    var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
-});
